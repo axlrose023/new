d7dee9a98e378b36aac09c0534619bcebe431db6
axlrose023 <97950293+axlrose023@users.noreply.github.com>
2024-06-26 13:02:06 +0300

    Update fake_account.py

--- backend/api/dto/fake_account.py
+++ backend/api/dto/fake_account.py
@@ -11,10 +11,12 @@ class FakeAccount:
     password: str
     cookies: list[dict]
     proxy_ip: str
+    proxy_username: str
+    proxy_password: str
     groups: list[GroupDTO]
 
     new_cookies: list[dict] | None = None
 
     @property
     def proxy_url(self) -> str:
-        return f"http://{settings.PROXY_USERNAME}:{settings.PROXY_PASSWORD}@{self.proxy_ip}"
+        return f"http://{self.proxy_username}:{self.proxy_password}@{self.proxy_ip}"
